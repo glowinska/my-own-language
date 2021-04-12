b7e8430658c364992eee6a9530ba50375a91cd73
glowinska <anna.glowinska98@gmail.com>
2021-04-12 22:50:58 +0200

    main.java with code

---
+++ grammar/llvm/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import com.company.grammar.GrammarLexer;
+
+import com.company.grammar.GrammarParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String code = "";
+        GrammarLexer lexer = new GrammarLexer(CharStreams.fromString(code));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        GrammarParser parser = new GrammarParser(tokens);
+        ParseTree tree = parser.prog();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        MyGrammarListener listener = new MyGrammarListener();
+        walker.walk(listener, tree);
+        System.out.println(listener.getResult());
+    }
+}
