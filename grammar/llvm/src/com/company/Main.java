a604ed56bba97396b2c3e3a70abc2a59e819c35b
glowinska <anna.glowinska98@gmail.com>
2021-04-13 15:16:21 +0200

    tests of reading & printing numbers

--- grammar/llvm/src/com/company/Main.java
+++ grammar/llvm/src/com/company/Main.java
@@ -12,11 +12,24 @@ public class Main {
 
     public static void main(String[] args) {
         String code = """
-        a = 5
-        print a
-        
-        b = 5 + 5
-        print b""";
+                a = 5
+                print a
+                
+                b = 5 + 5
+                print b
+                
+                c = 5.5 + 0.05
+                print c
+                
+                d = 3.14
+                print d
+                
+                readi e
+                print e
+                
+                f = 2.5 * 4.5
+                print f
+                """;
         GrammarLexer lexer = new GrammarLexer(CharStreams.fromString(code));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         GrammarParser parser = new GrammarParser(tokens);
